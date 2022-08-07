3c5f5380f2d8031382342db11f86a14742fffe4a
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2022-08-07 19:32:15 +0530

    Added Netlify URL

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const calculator = async () => {
   };
   try {
     const response = await axios.get(
-      `http://api.mathjs.org/v4/?expr=${expression}`,
+      `https://api.mathjs.org/v4/?expr=${expression}`,
       config
     );
     addingHistory();
